a612fb88b8deaf008d7662d88df324018e5e62d0
Lennox <omondilenox@outlook.com>
2020-09-07 13:43:39 +0300

    added docstring to text credectials

--- test_Credentials.py
+++ test_Credentials.py
@@ -2,5 +2,8 @@ from unittest import TestCase
 
 
 class Test(TestCase):
+    """
+    The test will be done in User class because it extends Credentials
+    """
     def test_credentials(self):
         self.fail()
