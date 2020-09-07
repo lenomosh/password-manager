2ff7fdf85e2b46ebe0410d62256568d83b65756c
Lennox <omondilenox@outlook.com>
2020-09-07 14:21:06 +0300

    finished readme, reran notebook cells, fixed PEP8

--- solution.js
+++
@@ -1,9 +0,0 @@
-const validBraces = braces =>{
-  if(braces.length%2 !==0) return false
-  const brace_length = braces.length/2
-  for(let i=0;i<brace_length;i++){
-    braces=braces.replace(/\[\]|\(\)|\{\}/gi,'')
-    if (braces.length===0) return true
-  }
-  return false
-}
